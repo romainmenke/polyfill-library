e31b76756598800bad47df78a9d747e9bde30b6f
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-31 16:01:49 +0000

    Add tests for ArrayBuffer.prototype.slice

--- polyfills/ArrayBuffer/tests.js
+++ polyfills/ArrayBuffer/tests.js
@@ -1,5 +1,5 @@
 /* eslint-env mocha */
-/* globals proclaim, ArrayBuffer, Symbol */
+/* globals proclaim, ArrayBuffer, Symbol, SharedArrayBuffer */
 
 describe('constructor', function () {
     it('is a function', function () {
@@ -147,3 +147,272 @@ describe('ArrayBuffer.isView', function () {
     });
 });
 
+describe('ArrayBuffer.prototype.slice', function () {
+    it('is a function', function () {
+        proclaim.isFunction(ArrayBuffer.prototype.slice);
+    });
+
+    it('has correct arity', function () {
+        proclaim.arity(ArrayBuffer.prototype.slice, 2);
+    });
+
+    it('has correct name', function () {
+        proclaim.hasName(ArrayBuffer.prototype.slice, 'slice');
+    });
+
+    it('is not enumerable', function () {
+        proclaim.isNotEnumerable(ArrayBuffer.prototype, 'slice');
+    });
+
+    it('throws TypeError if `this` does not have an [[ArrayBufferData]] internal slot', function () {
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call({});
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call([]);
+        }, TypeError);
+    });
+
+    it('throws a TypeError if `this` is a SharedArrayBuffer', function () {
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(new SharedArrayBuffer(0));
+        }, TypeError);
+    });
+
+    it('throws TypeError if `this` is not an Object', function () {
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(undefined);
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(null);
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(true);
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call("");
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(Symbol());
+        }, TypeError);
+
+        proclaim.throws(function () {
+            ArrayBuffer.prototype.slice.call(1);
+        }, TypeError);
+    });
+
+    it('if `end` argument is absent, end is set to [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5).byteLength, 4);
+    });
+
+    it('if `end` argument is undefined, end is set to [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5, undefined).byteLength, 4);
+    });
+
+    it('if `end` argument is larger than [[ArrayBufferByteLength]], end is set to [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5, 100).byteLength, 4);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5, Infinity).byteLength, 4);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5, 0x100000).byteLength, 4);
+    });
+
+    it('is extensible', function () {
+        proclaim.isTrue(Object.isExtensible(ArrayBuffer.prototype.slice));
+    });
+
+    it('if `end` is negative, it is subtracted from [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(4, -2).byteLength, 3);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(4, -10).byteLength, 0);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(4, -Infinity).byteLength, 0);
+    });
+
+    it('the `start` argument is converted to an integer', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(4.5).byteLength, 5);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(NaN).byteLength, 9);
+    });
+
+    it('the `end` argument is converted to an integer', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(0, 4.5).byteLength, 4);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(0, NaN).byteLength, 0);
+    });
+
+    it('if `start` is negative, it is relative to [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(-5, 6).byteLength, 2);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(-10, 6).byteLength, 6);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(-Infinity, 6).byteLength, 2);
+    });
+
+    it('throws a TypeError if called with `new`', function () {
+        proclaim.throws(function () {
+            new Array.prototype.slice();
+        });
+    });
+
+    it('does not have a prototype property', function () {
+        proclaim.isFalse(Object.prototype.hasOwnProperty.call(ArrayBuffer.prototype.slice, "prototype"));
+    });
+
+    it('throws TypeError if `constructor` property is not an object', function () {
+        proclaim.throws(function () {
+            var a = new ArrayBuffer();
+            a.constructor = null;
+            a.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var a = new ArrayBuffer();
+            a.constructor = true;
+            a.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var a = new ArrayBuffer();
+            a.constructor = "";
+            a.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var a = new ArrayBuffer();
+            a.constructor = 1;
+            a.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var a = new ArrayBuffer();
+            a.constructor = Symbol();
+            a.slice();
+        }, TypeError);
+    });
+
+    it('Uses ArrayBuffer constructor as the constructor if `constructor` property is undefined', function () {
+        var arrayBuffer = new ArrayBuffer();
+        arrayBuffer.constructor = undefined;
+        proclaim.deepStrictEqual(Object.getPrototypeOf(arrayBuffer.slice()), ArrayBuffer.prototype);
+    });
+
+    it('throws a TypeError if species constructor is not a constructor function', function () {
+        var speciesConstructor = {};
+        speciesConstructor[Symbol.species] = {};
+        var arrayBuffer = new ArrayBuffer();
+        arrayBuffer.constructor = speciesConstructor;
+        proclaim.throws(function () {
+            arrayBuffer.slice();
+        }, TypeError);
+    });
+
+    it('throws a TypeError if species constructor is not an object', function () {
+
+        proclaim.throws(function () {
+            var speciesConstructor = {};
+            speciesConstructor[Symbol.species] = true;
+            var arrayBuffer = new ArrayBuffer();
+            arrayBuffer.constructor = speciesConstructor;
+            arrayBuffer.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var speciesConstructor = {};
+            speciesConstructor[Symbol.species] = "";
+            var arrayBuffer = new ArrayBuffer();
+            arrayBuffer.constructor = speciesConstructor;
+            arrayBuffer.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var speciesConstructor = {};
+            speciesConstructor[Symbol.species] = Symbol();
+            var arrayBuffer = new ArrayBuffer();
+            arrayBuffer.constructor = speciesConstructor;
+            arrayBuffer.slice();
+        }, TypeError);
+
+        proclaim.throws(function () {
+            var speciesConstructor = {};
+            speciesConstructor[Symbol.species] = 1;
+            var arrayBuffer = new ArrayBuffer();
+            arrayBuffer.constructor = speciesConstructor;
+            arrayBuffer.slice();
+        }, TypeError);
+    });
+
+    it('uses ArrayBuffer constructor if species constructor is null', function () {
+        var speciesConstructor = {};
+        speciesConstructor[Symbol.species] = null;
+        var arrayBuffer = new ArrayBuffer();
+        arrayBuffer.constructor = speciesConstructor;
+        proclaim.deepStrictEqual(Object.getPrototypeOf(arrayBuffer.slice()), ArrayBuffer.prototype);
+    });
+
+    it('uses ArrayBuffer constructor if species constructor is undefined', function () {
+        var speciesConstructor = {};
+        speciesConstructor[Symbol.species] = undefined;
+        var arrayBuffer = new ArrayBuffer();
+        arrayBuffer.constructor = speciesConstructor;
+        proclaim.deepStrictEqual(Object.getPrototypeOf(arrayBuffer.slice()), ArrayBuffer.prototype);
+    });
+
+    it('if `start` is absent, it defaults to 0', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice().byteLength, 9);
+    });
+
+    it('if `start` is undefined, it defaults to 0', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(undefined).byteLength, 9);
+    });
+
+    it('returns 0 sized buffer is `start` is greater than `end`', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(5, 4).byteLength, 0);
+    });
+
+    it('if `start` is greater than [[ArrayBufferByteLength]], `start` is set to [[ArrayBufferByteLength]]', function () {
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(10, 5).byteLength, 0);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(Infinity, 5).byteLength, 0);
+        proclaim.deepStrictEqual(new ArrayBuffer(9).slice(0x10000, 5).byteLength, 0);
+    });
+
+    it('throws a TypeError if new ArrayBuffer is too small', function () {
+        proclaim.throws(function () {
+            var arrayBuffer = new ArrayBuffer(9);
+            arrayBuffer.constructor = {};
+            arrayBuffer.constructor[Symbol.species] = function () {
+                return new ArrayBuffer();
+            };
+            arrayBuffer.slice();
+        }, TypeError);
+    });
+
+    it('throws a TypeError if species constructor returns `this` value', function () {
+        proclaim.throws(function () {
+            var arrayBuffer = new ArrayBuffer(9);
+            arrayBuffer.constructor = {};
+            arrayBuffer.constructor[Symbol.species] = function () {
+                return arrayBuffer;
+            };
+            arrayBuffer.slice();
+        }, TypeError);
+    });
+
+    it('throws a TypeError if new object is not an ArrayBuffer instance', function () {
+        proclaim.throws(function () {
+            var arrayBuffer = new ArrayBuffer(9);
+            arrayBuffer.constructor = {};
+            arrayBuffer.constructor[Symbol.species] = function () {
+                return {};
+            };
+            arrayBuffer.slice();
+        }, TypeError);
+    });
+
+    it('does not throw a TypeError if new ArrayBuffer is too large', function () {
+        var arrayBuffer = new ArrayBuffer(8);
+        arrayBuffer.constructor = {};
+        arrayBuffer.constructor[Symbol.species] = function () {
+            return new ArrayBuffer(10);
+        };
+
+        proclaim.deepStrictEqual(arrayBuffer.slice().byteLength, 10);
+    });
+});
