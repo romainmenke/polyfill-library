42c5c8c12a069239dabac8a02a0f8dad0a1596f5
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-31 16:01:29 +0000

    Add tests for ArrayBuffer.isView

--- polyfills/ArrayBuffer/tests.js
+++ polyfills/ArrayBuffer/tests.js
@@ -45,3 +45,105 @@ describe('constructor', function () {
     });
 });
 
+describe('ArrayBuffer.isView', function () {
+    it('is a function', function () {
+        proclaim.isFunction(ArrayBuffer.isView);
+    });
+
+    it('has correct arity', function () {
+        proclaim.arity(ArrayBuffer.isView, 1);
+    });
+
+    it('has correct name', function () {
+        proclaim.hasName(ArrayBuffer.isView, 'isView');
+    });
+
+    it('is not enumerable', function () {
+        proclaim.isNotEnumerable(ArrayBuffer, 'isView');
+    });
+
+    it('returns false when given no argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView());
+    });
+
+    it('returns false when given an array as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView([]));
+    });
+
+    it('returns false when given an object as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView({}));
+    });
+
+    it('returns false when given null as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(null));
+    });
+
+    it('returns false when given undefined as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(undefined));
+    });
+
+    it('returns false when given number as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(1));
+    });
+
+    it('returns false when given string as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView('d'));
+    });
+
+    it('returns false when given RegExp as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(/./));
+    });
+
+    it('returns false when given ArrayBuffer as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(new ArrayBuffer));
+    });
+
+    it('returns false when given function as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(function () {}));
+    });
+
+    it('returns false when given boolean as argument', function () {
+        proclaim.isFalse(ArrayBuffer.isView(false));
+    });
+
+    it('returns true when given Int8Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Int8Array));
+    });
+
+    it('returns true when given Uint8Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Uint8Array));
+    });
+
+    it('returns true when given Uint8ClampedArray as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Uint8ClampedArray));
+    });
+
+    it('returns true when given Int16Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Int16Array));
+    });
+
+    it('returns true when given Uint16Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Uint16Array));
+    });
+
+    it('returns true when given Int32Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Int32Array));
+    });
+
+    it('returns true when given Uint32Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Uint32Array));
+    });
+
+    it('returns true when given Float32Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Float32Array));
+    });
+
+    it('returns true when given Float64Array as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new Float64Array));
+    });
+
+    it('returns true when given DataView as argument', function () {
+        proclaim.isTrue(ArrayBuffer.isView(new DataView(new ArrayBuffer)));
+    });
+});
+
