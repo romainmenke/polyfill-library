ea8c86e84c711328b4185205bd23cb8eb7854115
Jake Champion <me@jakechampion.name>
2020-01-09 14:40:30 +0000

    Ensure Array.prototype.find Indexdoes not skip holes and is unscopable

--- polyfills/Array/prototype/findIndex/detect.js
+++ polyfills/Array/prototype/findIndex/detect.js
@@ -1,1 +1,7 @@
-'findIndex' in Array.prototype
+'findIndex' in Array.prototype && (function () {
+    var skipsHoles = true;
+    Array(1).findIndex(function () {
+        return skipsHoles = false;
+    });
+    return !skipsHoles && Array.prototype[Symbol.unscopables].findIndex
+}())
