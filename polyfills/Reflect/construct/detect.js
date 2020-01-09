90321948f4edcdf2a9162231c9bc2e9985dd513e
Jake Champion <me@jakechampion.name>
2020-01-09 15:07:42 +0000

    Test Reflect.construct throws error if argumentsList is not supplied

--- polyfills/Reflect/construct/detect.js
+++ polyfills/Reflect/construct/detect.js
@@ -1,1 +1,12 @@
-'construct' in Reflect
+'construct' in Reflect && (function () {
+    try {
+        return !Reflect.construct(function () {
+            /* empty */ });
+    } catch (error) {
+        /* empty */ }
+
+    function F() {
+        /* empty */ }
+    return Reflect.construct(function () {
+        /* empty */ }, [], F) instanceof F;
+}())
