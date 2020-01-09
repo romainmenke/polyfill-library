021e3f6abe16231873595c3b01f1cbe159dab8ba
Jake Champion <me@jakechampion.name>
2020-01-09 14:53:49 +0000

    Ensure Symbol works with String() and JSON.stringify correctly and that it has all the methods and symbols we expect it to have

--- polyfills/Symbol/detect.js
+++ polyfills/Symbol/detect.js
@@ -1,1 +1,17 @@
-'Symbol' in this && this.Symbol.length === 0
+'Symbol' in this && this.Symbol.length === 0 && (function () {
+    try {
+        return String(Symbol()) && Object.getOwnPropertySymbols &&
+            Object.getOwnPropertySymbols('test') &&
+            Symbol['for'] &&
+            Symbol.keyFor &&
+            JSON.stringify([Symbol()]) == '[null]' &&
+            JSON.stringify({
+                a: Symbol()
+            }) == '{}' &&
+            JSON.stringify(Object(Symbol())) == '{}' &&
+            Symbol.prototype[Symbol.toPrimitive] &&
+            Symbol.prototype[Symbol.toStringTag];
+    } catch (err) {
+        return false;
+    }
+})()
