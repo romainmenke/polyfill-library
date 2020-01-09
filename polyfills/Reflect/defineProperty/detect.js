69acb975769a8e6f36baec24582a51958420ad8a
Jake Champion <me@jakechampion.name>
2020-01-09 15:10:42 +0000

    Check Reflect.defineProperty returns false if the property is already defined

--- polyfills/Reflect/defineProperty/detect.js
+++ polyfills/Reflect/defineProperty/detect.js
@@ -1,1 +1,14 @@
-'defineProperty' in Reflect
+"defineProperty" in Reflect &&
+  (function() {
+    try {
+      return !Reflect.defineProperty(
+        Object.defineProperty({}, 1, { value: 1 }),
+        1,
+        {
+          value: 2
+        }
+      );
+    } catch (e) {
+      return false;
+    }
+  })();
