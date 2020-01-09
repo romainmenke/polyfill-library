e75058d47ff5b9871a35150e2920d8205cd88e8d
Jake Champion <me@jakechampion.name>
2020-01-09 14:44:28 +0000

    Ensure Array.prototype.reduce fails if context is null or undefined

--- polyfills/Array/prototype/reduce/detect.js
+++ polyfills/Array/prototype/reduce/detect.js
@@ -1,1 +1,7 @@
-'reduce' in Array.prototype
+'reduce' in Array.prototype && (function () {
+    try {
+      Array.prototype.reduce.call(null, function () { /* empty */ }, 1);
+    } catch (error) {
+      return true;
+    }
+  }())
