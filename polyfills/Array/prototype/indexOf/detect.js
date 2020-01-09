71968a8f3be69b2ceeec4c2a0106c2c6ff3a295c
Jake Champion <me@jakechampion.name>
2020-01-09 14:43:44 +0000

    Ensure Array.prototype.indexOf fails if context is null or undefined and works with negative fromIndex values correctly

--- polyfills/Array/prototype/indexOf/detect.js
+++ polyfills/Array/prototype/indexOf/detect.js
@@ -1,1 +1,7 @@
-'indexOf' in Array.prototype
+'indexOf' in Array.prototype && (function () {
+    try {
+        Array.prototype.indexOf.call(null);
+    } catch (error) {
+        return 1 / [1].indexOf(1, -0) > 0;
+    }
+}())
