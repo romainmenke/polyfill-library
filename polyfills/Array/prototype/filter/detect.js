1ffd6dea5eb13740074e56dea41331c7c16f2b43
Jake Champion <me@jakechampion.name>
2020-01-09 14:38:41 +0000

    Ensure Array.prototype.filter works with species correctly

--- polyfills/Array/prototype/filter/detect.js
+++ polyfills/Array/prototype/filter/detect.js
@@ -1,1 +1,10 @@
-'filter' in Array.prototype
+'filter' in Array.prototype && (function () {
+    const array = [];
+    const constructor = array.constructor = {};
+    constructor[Symbol.species] = function () {
+        return {
+            foo: 1
+        };
+    };
+    return array.filter(Boolean).foo === 1;
+}())
