97470492a0399e681596262cf8173ea9ec3d66ff
Jake Champion <me@jakechampion.name>
2020-01-09 14:40:01 +0000

    Ensure Array.prototype.find does not skip holes and is unscopable

--- polyfills/Array/prototype/find/detect.js
+++ polyfills/Array/prototype/find/detect.js
@@ -1,1 +1,7 @@
-'find' in Array.prototype
+'find' in Array.prototype && (function () {
+    var skipsHoles = true;
+    Array(1).find(function () {
+        return skipsHoles = false;
+    });
+    return !skipsHoles && Array.prototype[Symbol.unscopables].find;
+}())
