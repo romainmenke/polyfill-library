2078daa2239564a1f7e64f9ffd0b2df75e7f62ac
Jake Champion <me@jakechampion.name>
2020-01-09 14:44:06 +0000

    Ensure Array.prototype.lastIndexOf fails if context is null or undefined and works with negative fromIndex values correctly

--- polyfills/Array/prototype/lastIndexOf/detect.js
+++ polyfills/Array/prototype/lastIndexOf/detect.js
@@ -1,1 +1,7 @@
-'lastIndexOf' in Array.prototype
+'lastIndexOf' in Array.prototype && (function () {
+    try {
+        Array.prototype.lastIndexOf.call(null);
+    } catch (error) {
+        return 1 / [1].lastIndexOf(1, -0) > 0;
+    }
+}())
