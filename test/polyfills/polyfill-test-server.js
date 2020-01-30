bb4155cf331ca9466815e5efa282b51022c45d33
Jake Champion <me@jakechampion.name>
2020-01-30 19:05:11 +0000

    k

--- test/polyfills/polyfill-test-server.js
+++ test/polyfills/polyfill-test-server.js
@@ -17,53 +17,65 @@ function createPolyfillLibraryConfigFor(features) {
   return features.split(",").reduce((config, feature) => {
     return Object.assign(config, {
       [feature]: {
-        flags: new Set()
+        flags: new Set(['always'])
       }
     });
   }, {});
 }
 
 // TODO: Make the root path contain a collection of links which run the tests for a specific feature
-// TODO: Have a toggle on the test pages to enable/disable the polyfill 
+// TODO: Have a toggle on the test pages to enable/disable the polyfill
 // TODO: Have a toggle on the test pages to load the polyfill with the `always` flag
 const server = http.createServer(async (request, response) => {
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    if (url.pathname.startsWith('/favicon.ico')) {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    const featureToTest = url.searchParams.get('features');
-  
-    try {
-      // TODO: deal with aliases and multiple features to test
-      const polyfillMeta = await polyfillLibrary.describePolyfill(featureToTest);
-      if (!polyfillMeta || !polyfillMeta.hasTests) {
-        response.writeHead(404);
-        response.end();
-      } else {
-        const tests = fs.readFileSync(
-          path.join(__dirname, `../../polyfills/${featureToTest.replace(/\./g, '/')}/tests.js`),
-          "utf-8"
-        );
-        const shoudlWeLoadPolyfill = url.searchParams.has("includePolyfill");
-        let polyfill = streamFromString("// Not loading polyfill\n//to load the polyfill add `includePolyfill` to the querystring");
-        if (shoudlWeLoadPolyfill) {
-          const params = {
-            features: createPolyfillLibraryConfigFor(
-              featureToTest
-            ),
-            minify: false,
-            stream: true,
-            uaString: request.headers["user-agent"]
-          };
-  
-          polyfill = polyfillLibrary.getPolyfillString(params);
-        }
-        const output = mergeStream();
-  
-        output.add(
-          streamFromString(`<!doctype html>
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  if (url.pathname.startsWith("/favicon.ico")) {
+    response.writeHead(404);
+    response.end();
+    return;
+  }
+  const featureToTest = url.searchParams.get("features");
+  if (!featureToTest) {
+    response.writeHead(404);
+    response.end();
+    return;
+  }
+  try {
+    // TODO: deal with aliases and multiple features to test
+    const polyfillMeta = await polyfillLibrary.describePolyfill(featureToTest);
+    if (!polyfillMeta) {
+      response.writeHead(404);
+      response.write(`No polyfill called "${featureToTest}" exists within polyfill-library.`);
+      response.end();
+    } else if (!polyfillMeta.hasTests) {
+      response.writeHead(404);
+      response.write(`No tests for "${featureToTest}" exist within polyfill-library.`);
+      response.end();
+    } else {
+      const tests = fs.readFileSync(
+        path.join(
+          __dirname,
+          `../../polyfills/${featureToTest.replace(/\./g, "/")}/tests.js`
+        ),
+        "utf-8"
+      );
+      const shoudlWeLoadPolyfill = url.searchParams.has("includePolyfill");
+      let polyfill = streamFromString(
+        "// Not loading polyfill\n//to load the polyfill add `includePolyfill` to the querystring"
+      );
+      if (shoudlWeLoadPolyfill) {
+        const params = {
+          features: createPolyfillLibraryConfigFor(featureToTest),
+          minify: false,
+          stream: true,
+          uaString: request.headers["user-agent"]
+        };
+
+        polyfill = polyfillLibrary.getPolyfillString(params);
+      }
+      const output = mergeStream();
+
+      output.add(
+        streamFromString(`<!doctype html>
           <html>
               <head>
                   <meta charset="utf-8">
@@ -77,25 +89,25 @@ const server = http.createServer(async (request, response) => {
                       mocha.setup('bdd');
                       window.onload = function () { mocha.run(); };
                   </script>`)
-        );
-        output.add(streamFromString('<script type="text/javascript" >'));
-        output.add(polyfill);
-        output.add(streamFromString("</script>"));
-        output.add(
-          streamFromString(`<script type="text/javascript">${tests}</script>
+      );
+      output.add(streamFromString('<script type="text/javascript" >'));
+      output.add(polyfill);
+      output.add(streamFromString("</script>"));
+      output.add(
+        streamFromString(`<script type="text/javascript">${tests}</script>
               </body>
           </html>`)
-        );
-        response.setHeader("Content-Type", "text/html");
-        response.writeHead(200);
-        response.flushHeaders();
-        output.pipe(response);
-      }
-    } catch (e) {
-      console.error(e);
-      response.writeHead(404);
-      response.end();
+      );
+      response.setHeader("Content-Type", "text/html");
+      response.writeHead(200);
+      response.flushHeaders();
+      output.pipe(response);
     }
-  });
+  } catch (e) {
+    console.error(e);
+    response.writeHead(404);
+    response.end();
+  }
+});
 
-  module.exports = server;
+module.exports = server;
