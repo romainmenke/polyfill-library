9985a8d4e4bca6b2c04288b622bbfeb42d85ab93
Jake Champion <me@jakechampion.name>
2020-02-18 02:24:51 +0000

    fix lint issues

--- polyfills/Intl/PluralRules/tests.js
+++ polyfills/Intl/PluralRules/tests.js
@@ -6,9 +6,9 @@ describe('Intl.PluralRules', function () {
 
     if (Intl.PluralRules && typeof Intl.PluralRules.__addLocaleData === 'function') {
       Intl.PluralRules.__addLocaleData({
-        "locale": "en",
-        "categories": { "cardinal": ["one", "other"], "ordinal": ["one", "two", "few", "other"] },
-        "fn": function (n, ord) {
+        locale: "en",
+        categories: { cardinal: ["one", "other"], ordinal: ["one", "two", "few", "other"] },
+        fn: function (n, ord) {
           var s = String(n).split('.'), v0 = !s[1], t0 = Number(s[0]) == n,
             n10 = t0 && s[0].slice(-1), n100 = t0 && s[0].slice(-2);
           if (ord) return (n10 == 1 && n100 != 11) ? 'one'
