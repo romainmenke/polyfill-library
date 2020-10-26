8a80b63c09e1eea5048a28b4fbeda622dfcdb4fa
romainmenke <romainmenke@gmail.com>
2020-10-26 17:58:21 +0100

    try something

--- polyfills/smoothscroll/detect.js
+++ polyfills/smoothscroll/detect.js
@@ -1,1 +1,32 @@
-'document' in self && 'documentElement' in self.document && 'style' in self.document.documentElement && 'scrollBehavior' in document.documentElement.style;
+('document' in self && 'documentElement' in self.document && 'style' in self.document.documentElement && 'scrollBehavior' in document.documentElement.style) || (function () {
+	try {
+		var supportsSmoothScroll = false;
+		
+		var scrollOptions = {
+			top: 1,
+			left: 0
+		};
+		
+		Object.defineProperty(scrollOptions, 'behavior', {
+			get: function () {
+				supportsSmoothScroll = true;
+				return 'smooth';
+			},
+			
+			// Ensure this property lasts through cloning / destructuring:
+			enumerable: true
+		});
+		
+		var a = document.createElement('div');
+		var b = document.createElement('div');
+		a.setAttribute('style', 'height: 1px');
+		b.setAttribute('style', 'height: 2px');
+		
+		a.appendChild(b);
+		a.scrollTo(scrollOptions);
+		
+		return supportsSmoothScroll;
+	} catch (e) {
+		return false;
+	}
+})();
